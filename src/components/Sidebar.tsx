4b764c9eee15a62fa30fdbb7d6e601077fd7a5c1
claudiomss <claudiocld95@gmail.com>
2023-12-26 22:13:40 -0300

    build: ignite feed

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/claudiomss.png" />
+
+        <strong>Claudio Marcelo</strong>
+        <span>Front Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
